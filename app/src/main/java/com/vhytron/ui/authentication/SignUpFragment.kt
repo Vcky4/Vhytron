e4ff0b1b4192a39059078f7ede75874d7b1ec8d7
vicksoson <okon.victor.u@gmail.com>
2022-02-23 03:48:36 +0100

    first commit

---
+++ app/src/main/java/com/vhytron/ui/authentication/SignUpFragment.kt
@@ -0,0 +1,30 @@
+package com.vhytron.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.vhytron.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+}
