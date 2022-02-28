96a5768fa15d74b334df5a3d5c3a1e8aade3c93d
vicksoson <okon.victor.u@gmail.com>
2022-02-28 17:26:34 +0100

    fixes UI

--- app/src/main/java/com/vhytron/ui/ViewPagerAdapter.kt
+++ app/src/main/java/com/vhytron/ui/ViewPagerAdapter.kt
@@ -2,7 +2,6 @@ package com.vhytron.ui
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
